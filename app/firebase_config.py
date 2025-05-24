c3ca26343b66275b93119e0032ea4e2189105f65
Sreeram Sharma <84390989+seramsharma30@users.noreply.github.com>
2025-05-24 11:02:11 +0530

    Update firebase_config.py

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,10 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json, os
 
 def initialize_firebase():
     try:
         # Initialize Firebase with your credentials
-        cred = credentials.Certificate('fb.json')
+        cred_dict = json.loads(os.getenv("FIREBASE_KEY"))
+        cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
         
         # Get Firestore client
